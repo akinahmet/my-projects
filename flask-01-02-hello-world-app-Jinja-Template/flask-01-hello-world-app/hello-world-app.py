b5996e93d15dad6a39b90df140078bd4b7658f73
AKN4 <ahmtakn@protonmail.com>
2022-06-01 16:27:39 +0200

    modified

--- flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
+++ flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -1,24 +1,29 @@
 from flask import Flask 
-
+# flask localhost:5000 portunda calisir
 app = Flask(__name__) # app nesnesi olusturuyoruz
 
-@app.route("/")  # decorator
+@app.route("/")  # decorator, ana sayfada yazilacak olan yaziyi tanimliyoruz
 def head():
     return "Hello world from flask"
 
-@app.route("/second")
+@app.route("/second") # ikinci sayfaya
 def second():
     return "This is my second page"
 
-@app.route("/third/subthird")
+@app.route("/third/subthird") 
 def third():
-    return "<h2>This is the subpath of third page</h2>"
+    return "<h2>This is the subpath  of third page </h2>"
 
-@app.route("/forth/<string:id>")
+@app.route("/forth/<string:id>") 
 def forth(id):
     return f'Id of this page is {id}'
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
-    #app.run(debug=True, port=3000)
+
+
+
+
+if __name__ == "__main__": 
+    #app.run(host='0.0.0.0', port=80)
+    #app.run(debug=True, port=80) 
+    app.run(debug=True)  # default olarak port 5000, ama bir üst satirlardaki gibi farkli port yapabilirsin
